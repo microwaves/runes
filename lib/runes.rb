534343f533519d6ca6177227743ff37555de41c4
zanst <me@zan.st>
2011-04-05 14:21:28 -0300

    added logic to configuration

--- lib/runes.rb
+++ lib/runes.rb
@@ -1,3 +1,15 @@
-require 'runes/railtie'
+require 'rubberband'
+
+require File.dirname(__FILE__) + '/runes/railtie'
 require File.dirname(__FILE__) + '/runes/core'
+
 ActiveRecord::Base.extend Runes
+
+begin
+  config_path = Rails.root.to_s + '/config/runes.yml'
+  config = YAML.load_file(config_path)
+rescue
+  config = nil
+end
+
+$es_client = config.nil? ? ElasticSearch.new('127.0.0.1:9200') : ElasticSearch.new(config['host'], :timeout => 10)
