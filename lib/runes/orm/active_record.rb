c13ba718a075b60e13c1b56acfe00eaeb14b5534
zanst <me@zan.st>
2011-04-18 20:26:11 -0300

    adding more actions to AR extension

--- lib/runes/orm/active_record.rb
+++ lib/runes/orm/active_record.rb
@@ -2,7 +2,23 @@ module Runes
   module Orm
     module ActiveRecord
       def acts_as_indexable(options={})
-        Runes::Base.set_actor(self)
+        Runes::Base.set_actor(self.name)
+        send :include, InstanceMethods
+        after_destroy :destroy_object_from_index
+        after_save :add_object_to_index
+      end
+    end
+
+    module InstanceMethods
+      def delete_index
+      end
+
+      def destroy_object_from_index
+      end
+
+      def add_object_to_index
+        @class_name = self.name.underscore
+        $es_client.index(self.to_json, :index => @class_name, :type => @class_name)
       end
     end
   end
